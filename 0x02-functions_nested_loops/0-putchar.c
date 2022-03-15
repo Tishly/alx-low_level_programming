ad2f8253d6019ba45088b40f974cbd1d9927ccc5
Tishlyy <ekaetteessien27@gmail.com>
2022-03-15 12:15:48 +0100

    Print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "_putchar.h"
+/**
+ * main -  Main block
+ * Description: Print _putchar.c
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c[10] = "_putchar"
+
+		int i = 0;
+
+	while (i < 9)
+	{
+		_putchar(c[i]);
+		i++;
+	}
+	_putchar('\n');
+	return (0);
+}
