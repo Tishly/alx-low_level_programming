e7bcb19a28a0b5847b6e629e9c2594255a47d252
Tishlyy <ekaetteessien27@gmail.com>
2022-03-15 16:59:59 +0100

    Main

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - main block
+ * Description: Print alphabets in void
+ * Return: Always 0 (Success)
+ */
+
+void print alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <=  'z'; c++)
+	{
+		_putchar(c);
+	}
+
+	_putchar('\n');
+}
