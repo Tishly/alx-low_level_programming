84d242ca7ea92f568d2ff4e78a42c853b0843264
Tishlyy <ekaetteessien27@gmail.com>
2022-03-21 20:37:56 +0100

    Reset to 98

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * swap_int - swap variable values
+ * @n: pointer 1
+ * @f: pointer 2
+ * Return: void
+ */
+
+void swap_int(int *n; int *f)
+{
+	a* += *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
