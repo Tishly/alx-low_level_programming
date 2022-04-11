c651c5afdb7a8773605c3ffa7ab782ce70e357c5
Tishlyy <ekaetteessien27@gmail.com>
2022-04-11 17:00:09 +0100

    Calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 
 /**
@@ -39,7 +40,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (m == 0)
 		return (NULL);
 
-	*_memset(m, 0, nmemb * size);
+	_memset(m, 0, nmemb * size);
 
 	return (m);
 }
