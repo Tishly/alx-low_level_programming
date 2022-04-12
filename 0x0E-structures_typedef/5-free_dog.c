80915fb1703dcbf7c588d20095cca31eab6f6257
Tishlyy <ekaetteessien27@gmail.com>
2022-04-13 00:36:36 +0100

    Freedog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function that frees allocated space
+ * @d: pointer to struct
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
