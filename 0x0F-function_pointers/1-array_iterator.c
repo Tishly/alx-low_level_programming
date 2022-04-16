3349d259b5213b9bfb16734b113e32a6f3d89487
Tishlyy <ekaetteessien27@gmail.com>
2022-04-16 01:04:55 +0100

    iterate

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "funtion_pointers.h"
+
+/**
+ * array_iterator - function that iterats arrays
+ * @array: array
+ * @size: the size of the array
+ * @action: pointer to the function you need to use
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i = 0;
+
+	if (array[size] != NULL)
+	{
+		for (; i < size; i++)
+			action(array[i]);
+	}
+}
