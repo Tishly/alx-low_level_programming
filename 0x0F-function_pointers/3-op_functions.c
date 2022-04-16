f3cb2c28c51b69b814686432ea6fe99e0d25b38d
Tishlyy <ekaetteessien27@gmail.com>
2022-04-16 03:52:11 +0100

    comment update

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -34,22 +34,22 @@ int op_mul(int a, int b)
 }
 
 /**
- *  * op_div - division of 2 numbers
- *   * @a: first int
- *    * @b: second int
- *     * Return: result of the division of the numbers
- *      */
+ * op_div - division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: result of the division of the numbers
+ */
 int op_div(int a, int b)
 {
 		return (a / b);
 }
 
 /**
- *  * op_mod - get remainder of the division of 2 numbers
- *   * @a: first int
- *    * @b: second int
- *     * Return: remainder of division of the numbers
- *      */
+ * op_mod - get remainder of the division of 2 numbers
+ * @a: first int
+ * @b: second int
+ * Return: remainder of division of the numbers
+ */
 int op_mod(int a, int b)
 {
 		return (a % b);
