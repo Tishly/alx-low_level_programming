53df02a28251b487c4364bdda4cd34602a4b04a8
Tishlyy <ekaetteessien28@gmail.com>
2022-10-07 08:09:40 -0700

    Binary

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * binary_search - search for value in sorted arrays
+ * @array: pointer to the first element
+ * @size: size of the array
+ * @value: element to search for
+ *
+ * Rrturn: -1 if array is null or value absent, else return value
+ */
+
+int binary_search(int *array, size_t size, int value)
+{
+	unsigned int i;
+}
