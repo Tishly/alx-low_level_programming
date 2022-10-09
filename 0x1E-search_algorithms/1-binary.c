8f823714a4263d3812fb6c0a38559960a020cdfb
Tishlyy <ekaetteessien28@gmail.com>
2022-10-08 22:02:27 -0700

    Binary

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -12,5 +12,38 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-	unsigned int i;
+	int  left, right, pivot;
+
+	if (array == NULL)
+		return (-1);
+	left = 0;
+	right = size - 1;
+	while (left <= right)
+	{
+		print_array(array, left, right);
+		pivot = (left + right) / 2;
+		if (array[pivot] > value)
+			right = pivot - 1;
+		else if (array[pivot] < value)
+			left = pivot + 1;
+		else
+			return (pivot);
+	}
+	return (-1);
+}
+
+/**
+ * print_array - print array
+ * @array: array
+ * @left: starting print
+ * @right: ending print
+ */
+void print_array(int *array, int left, int right)
+{
+	int i;
+
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
 }
